b824a69dff97791ac1785a63ace9b14adddb02a8
Ted Morin <morinted@gmail.com>
2020-03-12 19:47:31 -0400

    Add trick checker

---
+++ bridge/tricks.js
@@ -0,0 +1,35 @@
+const { SUITS, compareSuits } = require('./suits')
+const { compareRanks } = require('./deck')
+
+const trickWinnerIndex = ([lead, second, third, fourth], trumpId) => {
+  const leadSuitId = lead.suitId
+  const useTrump = leadSuitId !== trumpId && trumpId !== SUITS.NOTRUMP.id // Possible for players to trump.
+
+  const isTrump = card => card.suitId === trumpId
+
+  const winner = [lead, second, third, fourth].reduce((winner, challenge) => {
+    // Check trump, if applicable.
+    if (useTrump) {
+      if (isTrump(challenge) && isTrump(winner)) {
+        return [challenge, winner].sort(compareCards)[1]
+      } else if (isTrump(challenge)) {
+        return challenge
+      } else if (isTrump(winner)) {
+        return winner
+      }
+    }
+
+    // Check for lay-off of other suit.
+    const challengeSuitId = challenge.suitId
+    if (leadSuitId !== challengeSuitId) {
+      return winner
+    }
+
+    // Compare rank.
+    return [winner, challenge].sort((a, b) => compareRanks(a.rank, b.rank))[1]
+  })
+
+  return [lead, second, third, fourth].indexOf(winner)
+}
+
+module.exports = { trickWinnerIndex }
---
+++ test/tricks.test.js
@@ -0,0 +1,99 @@
+const { trickWinnerIndex } = require('../bridge/tricks')
+const { createCard } = require('../bridge/deck')
+const { SUITS } = require('../bridge/suits')
+
+const club = SUITS.CLUBS.id
+const heart = SUITS.HEARTS.id
+const diamond = SUITS.DIAMONDS.id
+const spade = SUITS.SPADES.id
+const notrump = SUITS.NOTRUMP.id
+
+test('follow-suit trick', () => {
+  expect(
+    trickWinnerIndex(
+      [
+        createCard(club, '2'),
+        createCard(club, '3'),
+        createCard(club, 'K'),
+        createCard(club, '7'),
+      ],
+      notrump
+    )
+  ).toEqual(2)
+  expect(
+    trickWinnerIndex(
+      [
+        createCard(club, 'A'),
+        createCard(club, '3'),
+        createCard(club, 'K'),
+        createCard(club, '7'),
+      ],
+      club
+    )
+  ).toEqual(0)
+  expect(
+    trickWinnerIndex(
+      [
+        createCard(club, '2'),
+        createCard(club, '3'),
+        createCard(club, '4'),
+        createCard(club, 'Q'),
+      ],
+      heart
+    )
+  ).toEqual(3)
+})
+
+test('trump-in', () => {
+  expect(
+    trickWinnerIndex(
+      [
+        createCard(diamond, 'A'),
+        createCard(club, 'A'),
+        createCard(heart, '2'),
+        createCard(spade, 'A'),
+      ],
+      heart
+    )
+  ).toEqual(2)
+})
+
+test('no trumps', () => {
+  expect(
+    trickWinnerIndex(
+      [
+        createCard(diamond, 'A'),
+        createCard(club, 'A'),
+        createCard(heart, '2'),
+        createCard(spade, 'A'),
+      ],
+      notrump
+    )
+  ).toEqual(0)
+
+  expect(
+    trickWinnerIndex(
+      [
+        createCard(diamond, '7'),
+        createCard(club, 'A'),
+        createCard(spade, '2'),
+        createCard(spade, 'A'),
+      ],
+      heart
+    )
+  ).toEqual(0)
+})
+
+test('all trumps', () => {
+  expect(
+    trickWinnerIndex(
+      [
+        createCard(diamond, '7'),
+        createCard(diamond, '2'),
+        createCard(diamond, '8'),
+        createCard(diamond, '3'),
+      ],
+      diamond
+    )
+  ).toEqual(2)
+})
